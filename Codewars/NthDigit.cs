468e1426301287dfd539ce7ac5b0ea0d3bf5beb2
frank.lamo <frank.lamo@ukr.net>
2018-01-15 01:03:10 +0200

    Added solution for "Find n'th Digit of a Number"

---
+++ Codewars/NthDigit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /*The function findDigit takes two numbers as input, num and nth.It outputs the nth digit of num(counting from right to left).
+       Note:
+       If num is negative, ignore its sign and treat it as a positive value.
+       If nth is not positive, return -1.
+       Keep in mind that 42 = 00042. This means that findDigit(42, 5) would return 0.*/
+    public static class NthDigit
+    {
+        public static int FindDigit(int num, int nth)
+        {
+            if (nth <= 0)
+            {
+                return -1;
+            }
+            char[] aschar = Math.Abs(num).ToString().Reverse().ToArray();
+
+            if (nth<=aschar.Length)
+            {
+                return int.Parse(aschar[nth-1].ToString());
+            }
+            else
+            {
+                return 0;
+            }
+           
+        }
+    }
+}
--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -10,13 +10,7 @@ namespace Codewars
     {
         static void Main(string[] args)
         {
-            List<int> numbers = PascalsTriangle.PascalTriangle(20);
-
-            foreach (var item in numbers)
-            {
-                Console.Write(item + " ");
-            }
-            // Console.WriteLine(TheOldSwitcheroo.Vowel2Index("this is my string"));
+            Console.WriteLine(NthDigit.FindDigit(-456, 4));
         
             Console.ReadKey();
         }
