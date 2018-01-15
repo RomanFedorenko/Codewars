6c30af897ea18dffebed4c8074e5a0d1d6718051
frank.lamo <frank.lamo@ukr.net>
2018-01-15 02:52:27 +0200

    Added solution for "Remove String Spaces" kata

---
+++ Codewars/RemoveSpaces.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    // Simple, remove the spaces from the string, then return the resultant string.
+    public static class RemoveSpaces
+    {
+        public static string NoSpace(string input)
+        {
+            return input.Replace(" ", string.Empty);
+        }
+    }
+}
