11468e3729f8e81e5839320bde49ba704d14711e
frank.lamo <frank.lamo@ukr.net>
2018-01-15 02:25:51 +0200

    Added solution for "Triangular Treasure" kata

--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -10,7 +10,7 @@ namespace Codewars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(NthDigit.FindDigit(-456, 4));
+            Console.WriteLine(TriangularTreasure.Triangular(3));
         
             Console.ReadKey();
         }
---
+++ Codewars/TriangularTreasure.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    /*Triangular numbers are so called because of the equilateral triangular shape that they occupy when laid out as dots.i.e.
+
+            1st(1)   2nd(3)    3rd(6)
+            *        **        ******
+                     *         ***
+                               *
+    You need to return the nth triangular number.You should return 0 for out of range values:*/
+    public static class TriangularTreasure
+    {
+
+        public static int Triangular(int n)
+        {
+                     
+            return n>0 ? (n * (n + 1)) / 2 : 0;
+
+        }
+    }
+}
