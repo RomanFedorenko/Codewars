ab0184f637eb5bd94db1afdb2dfbfc0ebddb88ba
Roman Fedorenko <frank.lamo@ukr.net>
2018-10-01 23:50:33 +0300

    Added solution for "Dubstep" kata

--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -8,7 +8,7 @@ namespace Codewars
         static void Main(string[] args)
         {
             // Console.WriteLine(PopulationGrowth.NbYear(1500000, 0, 10000, 2000000));
-            Console.WriteLine(SumOfDigitsDigitalRoot.Persistance(4));
+            Console.WriteLine(Dubstep.SongDecoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB"));
             Console.ReadKey();
         }
     }
