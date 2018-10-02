59ca3745d4ec23f29f3aa32712be910e039f7ba3
Roman Fedorenko <frank.lamo@ukr.net>
2018-10-03 00:49:41 +0300

    Added solution for "Find the odd int" kata

---
+++ Codewars/Katas/FindTheOddInt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars.Katas
+{
+    public static class FindTheOddInt
+    {
+        // Given an array, find the int that appears an odd number of times.
+        // There will always be only one integer that appears an odd number of times.
+        public static int find_it(int[] seq)
+        {            
+            return seq.FirstOrDefault(x => seq.Count(y => y == x) % 2 != 0);
+        }
+    }
+}
--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -9,7 +9,8 @@ namespace Codewars
         static void Main(string[] args)
         {
             // Console.WriteLine(PopulationGrowth.NbYear(1500000, 0, 10000, 2000000));
-            Console.WriteLine(WhoLikeIt.Likes(new string[] { "Peter" }));
+            Console.WriteLine(FindTheOddInt.find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }));
+
             Console.ReadKey();
         }
     }
